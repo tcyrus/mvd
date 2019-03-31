32f3922cd7e7997ccf5f7d43915e73c28d7dcadc
Timothy Cyrus <4201229+tcyrus@users.noreply.github.com>
2019-03-31 16:44:23 -0400

    Update bin.js

--- bin.js
+++ bin.js
@@ -1,35 +1,34 @@
-var fs = require('fs')
-var path = require('path')
-var ssbKeys = require('ssb-keys')
-var stringify = require('pull-stringify')
-var open = require('opn')
-var home = require('os-homedir')()
-var nonPrivate = require('non-private-ip')
-var muxrpcli = require('muxrpcli')
+const fs = require('fs'),
+      path = require('path'),
+      ssbKeys = require('ssb-keys'),
+      stringify = require('pull-stringify'),
+      open = require('opn'),
+      home = require('os-homedir')(),
+      nonPrivate = require('non-private-ip'),
+      muxrpcli = require('muxrpcli')
 
-var SEC = 1e3
-var MIN = 60*SEC
+const SEC = 1e3
+const MIN = 60*SEC
 
-var network = 'ssb'
-//var network = 'decent'
-//var network = 'testnet'
+const network = 'ssb'
+//const network = 'decent'
+//const network = 'testnet'
 
-var config = require('./config/inject')(network)
+let config = require('./config/inject')(network)
 
 config.keys = ssbKeys.loadOrCreateSync(path.join(config.path, 'secret'))
 
-var mvdClient = fs.readFileSync(path.join('./build/index.html'))
+const mvdClient = fs.readFileSync(path.join('./build/index.html'))
 
-var manifestFile = path.join(config.path, 'manifest.json')
+const manifestFile = path.join(config.path, 'manifest.json')
 
-var argv = process.argv.slice(2)
-var i = argv.indexOf('--')
-var conf = argv.slice(i+1)
+let argv = process.argv.slice(2)
+const i = argv.indexOf('--')
+const conf = argv.slice(i+1)
 argv = ~i ? argv.slice(0, i) : argv
 
-if (argv[0] == 'server') {
-  
-  var createSbot = require('scuttlebot-release/node_modules/scuttlebot')
+if (argv[0] === 'server') {
+  const createSbot = require('scuttlebot-release/node_modules/scuttlebot')
     .use(require('scuttlebot-release/node_modules/scuttlebot/plugins/master'))
     .use(require('scuttlebot-release/node_modules/scuttlebot/plugins/gossip'))
     .use(require('scuttlebot-release/node_modules/scuttlebot/plugins/replicate'))
@@ -48,15 +47,15 @@ if (argv[0] == 'server') {
       version: '1.0.0',
       init: function (sbot) {
         sbot.ws.use(function (req, res, next) {
-          var send = config
+          const send = config
           delete send.keys // very important to keep this, as it removes the server keys from the config before broadcast
           send.address = sbot.ws.getAddress()
           sbot.invite.create({modern: true}, function (err, cb) {
             send.invite = cb
           })
-          if(req.url == '/')
+          if (req.url === '/')
             res.end(mvdClient)
-          if(req.url == '/get-config')
+          else if (req.url === '/get-config')
             res.end(JSON.stringify(send))
           else next()
         })
@@ -65,12 +64,12 @@ if (argv[0] == 'server') {
   
   open('http://localhost:' + config.ws.port, {wait: false})
   
-  var server = createSbot(config)
+  const server = createSbot(config)
   
   fs.writeFileSync(manifestFile, JSON.stringify(server.getManifest(), null, 2))
 } else {
 
-  var manifest
+  let manifest
   try {
     manifest = JSON.parse(fs.readFileSync(manifestFile))
   } catch (err) {
@@ -88,11 +87,11 @@ if (argv[0] == 'server') {
     caps: config.caps,
     key: config.key || config.keys.id
   }, function (err, rpc) {
-    if(err) {
+    if (err) {
       if (/could not connect/.test(err.message)) {
         console.log('Error: Could not connect to the scuttlebot server.')
         console.log('Use the "server" command to start it.')
-        if(config.verbose) throw err
+        if (config.verbose) throw err
         process.exit(1)
       }
       throw err
@@ -114,4 +113,3 @@ if (argv[0] == 'server') {
     muxrpcli(argv, manifest, rpc, config.verbose)
   })
 }
-
